6daa0106eec9257d51ad12a95c9dc48636e7a727
Jan-Henrik Hempel <jhh@j2h.com>
2015-09-23 21:57:31 +0200

    Revert "Bildgrößen verkleinert bzw. Retina Variante entfernt, da die Ladezeiten zu hoch werden"
    This reverts commit 31b0d0eddc732e8682d22fea353645a05f6b4590.

--- js/nak.js
+++ js/nak.js
@@ -111,17 +111,37 @@ function recalculate() {
 		pixelRatio = 1;
 		if (getDevicePixelRatio() > 1.5) pixelRatio = 2;
 		if( $(window).width() <= 549 ) {
-			pixelTeaserWidth = 520;
-			pixelFullWidth = 520;
+			if(pixelRatio==2){
+				pixelTeaserWidth = 960;
+				pixelFullWidth = 960;
+			}else{
+				pixelTeaserWidth = 520;
+				pixelFullWidth = 520;
+			}
 		}else if ( $(window).width() <= 991 ) {
-			pixelTeaserWidth = 520;
-			pixelFullWidth = 960;
+			if(pixelRatio==2){
+				pixelTeaserWidth = 960;
+				pixelFullWidth = 1200;
+			}else{
+				pixelTeaserWidth = 520;
+				pixelFullWidth = 960;
+			}
 		}else if ( $(window).width() <= 1199 ) {
-			pixelTeaserWidth = 520;
-			pixelFullWidth = 1000;
+			if(pixelRatio==2){
+				pixelTeaserWidth = 960;
+				pixelFullWidth = 1200;
+			}else{
+				pixelTeaserWidth = 520;
+				pixelFullWidth = 1200;
+			}
 		}else {
-			pixelTeaserWidth = 520;
-			pixelFullWidth = 1200;
+			if(pixelRatio==2){
+				pixelTeaserWidth = 960;
+				pixelFullWidth = 1600;
+			}else{
+				pixelTeaserWidth = 520;
+				pixelFullWidth = 1200;
+			}
 		}
 	}
 
@@ -159,14 +179,14 @@ function filter(tag){
 	if(tag=="Alle"){
 		$(".project-tile").each(function(n) {
 			console.log($(this).is(":hidden"));
-			if($(this).is(":hidden")) $(this).show(800);
+			if($(this).is(":hidden")) $(this).fadeIn(800);
 		});		
 	}else{
 		$(".project-tile").each(function(n) {
-		    if($(this).is(":visible") && !$(this).hasClass(tag)) $(this).hide(500);
+		    if($(this).is(":visible") && !$(this).hasClass(tag)) $(this).fadeOut(500);
 		});
 		$(".project-tile."+tag).each(function(n) {
-		    if($(this).is(":hidden")) $(this).show(800);
+		    if($(this).is(":hidden")) $(this).fadeIn(800);
 		});		
 	}
 }
