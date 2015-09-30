97d7414bfacb9827cd2d48fc227ba3d65b9fca95
Jan-Henrik Hempel <jhh@j2h.com>
2015-09-30 22:19:04 +0200

    Javascript Aggregation

---
+++ main.js
@@ -0,0 +1,8 @@
+---
+---
+{% include js/jquery-1.11.3.min.js %}
+{% include js/bootstrap.min.js %}
+{% include js/jquery.transit.min.js %}
+{% include js/jquery.mobile.custom.min.js %}
+{% include js/modernizr.custom.90320.js %}
+{% include js/nak.js %}
